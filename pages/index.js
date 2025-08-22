6cd628662709ca9664798a2dcb3d16c4e525e6fd
Birkan <birkanmail@gmail.com>
2025-08-22 19:59:33 +0300

    env revised

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,15 @@ export default function Home() {
 
     const featuredEvents = getFeaturedEvents();
 
+    const env = process.env.NODE_ENV;
+    const apiKey = process.env.apiKey;
+
+
     return (
         <div>
             <EventList items={featuredEvents} />
+            <p>{env}</p>
+            <p>{apiKey}</p>
         </div>
     )
 }
